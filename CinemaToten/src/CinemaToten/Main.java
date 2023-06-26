5f9b618ede13eceb0052956f028221ca04cb8d37
beatrizmo <b166623@dac.unicamp.br>
2023-06-26 13:34:15 -0300

    correcao da classe main(CinemaToten)

--- CinemaToten/src/CinemaToten/Main.java
+++ CinemaToten/src/CinemaToten/Main.java
@@ -4,15 +4,14 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 
 public class Main {
-	
+
 	public static void createCSVFile() {
-		
+
 		String fileName = "AssentosReservados.csv";
 
 		//Cria objeto do tipo file chamado "AssentosReservados.csv"
@@ -28,159 +27,35 @@ public class Main {
 			}
 		}
 	}
-	
-	//Lista todas os filmes ja instanciadas e que estao na lista de filmes
-	public static void listarFilmes(ArrayList<Filme> listaFilmes) {
-		
-		System.out.println("----- Lista de filmes disponíveis -----");
-		
-		for (Filme filme : listaFilmes) {
-			System.out.println(listaFilmes.indexOf(filme)+1 + ". " + filme.getTitulo());
-		}
-	}
 
-	//Lista todas as sessoes ja instanciadas e que estao na lista de sessoes
-	public static void listarSessoes(ArrayList<Sessao> listaSessoes, Filme filme) {
-		
-		System.out.println("----- Lista de sessões disponíveis -----");
-		
-		for (Sessao sessao : listaSessoes) {
-			System.out.println(listaSessoes.indexOf(sessao)+1 + ". " + sessao.getData() +" - "+ sessao.getHorario());
-		}
-	}
-
-	/*
-	//Exibe os filmes disponiveis para a selecao e encaminha para a escolha de sessao
-	public static void escolherFilme(ArrayList<Filme> listaFilmes,ArrayList<Sessao> listaSessoes, Reservar reserva) throws IOException {
-		int filmeIndex;
-		boolean filmeValido = false;
-		Filme filmeEscolhido = null;
-		
-		
-		listarFilmes(listaFilmes);
-		System.out.println("0. Voltar para o menu inicial");
-
-		do {
-			System.out.print("Escolher um filme: ");
-			filmeIndex = scanner.nextInt();
-
-			if (filmeIndex == 0) {
-				return;
-			}
-
-			for (Filme filme : listaFilmes) {
-				if (listaFilmes.indexOf(filme) == filmeIndex-1) {
-					filmeValido = true;
-					filmeEscolhido = filme;
-					break;
+	public static ArrayList<Sessao> criarSessoes(ArrayList<Filme> listaFilmes) {
+		ArrayList<String> listaDatas = new ArrayList<>();
+		ArrayList<String> listaHorarios = new ArrayList<>();
+		//Cria uma lista com as datas em que os filmes serão exibidos
+		listaDatas.add("28/06");
+		listaDatas.add("29/06");
+		listaDatas.add("30/06");
+		//Cria uma lista com os horarios que os filmes serao exibidos
+		listaHorarios.add("14h");
+		listaHorarios.add("16h");
+		listaHorarios.add("18h");
+		//Para cada data da listaDatas é criado uma sessao em cada um dos horarios
+		ArrayList<Sessao> listaSessoes = new ArrayList<>();
+		for (int i=0; i< listaFilmes.size(); i++) {
+			for (int j=0; j < listaDatas.size(); j++) {
+				for (int k=0; k < listaHorarios.size(); k++) {
+					Sessao sessao = new Sessao(listaFilmes.get(i), 6,5, listaHorarios.get(k), listaDatas.get(j));
+					listaSessoes.add(sessao);
 				}
 			}
-
-			if (!filmeValido) {
-				System.out.println("Escolha inválida. Tente novamente.");
-			}
-		} while (!filmeValido);
-
-		escolherSessao(scanner, listaSessoes, filmeEscolhido, reserva);
-	}
-	
-	//Escolher sessao com base no filme selecionado
-	public static void escolherSessao(ArrayList<Sessao> listaSessoes, Filme filme, Reservar reserva) throws IOException {
-		int escolha;
-		
-		ArrayList<Sessao> sessoesDisp = new ArrayList<>();
-		
-		//Procura todas as sessoes disponiveis para aquele filme
-		for (Sessao sessao : listaSessoes) {
-			if (sessao.getFilme().equals(filme))
-				sessoesDisp.add(sessao);
 		}
-		do {
-			listarSessoes(sessoesDisp, filme);
-			System.out.println("0. Voltar para menu inicial");
+		return listaSessoes;
 
-			System.out.print("Escolha uma sessão disponível: ");
-			escolha = scanner.nextInt();
-
-			if (escolha == 0) {
-				return;
-			}
-		} while (escolha < 0 || escolha > sessoesDisp.size());
-
-		// Processar a sessão escolhida
-		System.out.println("Você selecionou a sessão: " + sessoesDisp.get(escolha - 1));
-		escolherAssentos(scanner, sessoesDisp.get(escolha-1), reserva);
-	}
-	
-	//Escolhe um assento para uma sessao passada como parametro
-	public static void escolherAssentos(Sessao sessao, Reservar reserva) throws IOException {
-		
-		System.out.print("Digite o numero de ingressos meia-entrada você quer comprar: ");
-		int qntMeias = scanner.nextInt();
-		
-		System.out.print("Digite o numero de ingressos inteira você quer comprar: ");
-		int qntInteiras = scanner.nextInt();
-		int qntAssentos = qntMeias + qntInteiras;
-		
-	    ArrayList<String> listaAssentos = new ArrayList<>();
-	    scanner.nextLine(); //consumindo a quebra de linha
-	    
-	    for (int i = 0; i < qntAssentos; i++) {
-	        System.out.print("Digite qual o assento nº " + (i + 1) + ": ");
-	        
-	        String numAssento = scanner.nextLine();
-
-	        // Encontre o assento escolhido na lista de assentos
-	        Assento AssentoSelec = null;
-	        for (Assento assento : sessao.getListaAssentos()) {
-	            if (assento.getPosAssento().equals(numAssento)) {
-	            	AssentoSelec = assento;
-	                break;
-	            }
-	        }
-
-	        if (AssentoSelec != null && reserva.reservarAssento(AssentoSelec.getPosAssento(), sessao)) {
-	            System.out.println("Assento " + numAssento + " foi reservado.");
-	            listaAssentos.add(AssentoSelec.getPosAssento());
-	        } else {
-	            System.out.println("Assento " + numAssento + " não está disponível.");
-	            i--; // Tentar nova seleção.
-	        }
-	    }
-	    realizarCompra(qntMeias, qntInteiras, scanner, reserva, listaAssentos, sessao);
 	}
 
-	*/
-	
-	//Apos escolhido a sessao e o assento é feito a compra
-	
-	public static void realizarCompra(int qntMeias, int qntInteiras, Scanner scanner, Reservar reserva, ArrayList<String> listaAssentos, Sessao sessao) {
-		
-		System.out.print("Deseja finalizar a compra? (S/N): ");
-	    String finalizar = scanner.nextLine();
-	    System.out.print("Digite o nome do comprador: ");
-	    String nome = scanner.nextLine();
-
-	    if (finalizar.equalsIgnoreCase("S")) {
-	    	try {
-				double precoFinal = reserva.comprarAssentos(qntMeias, qntInteiras, nome, listaAssentos, sessao);
-				System.out.println("Compra realizada com sucesso! Valor: " + precoFinal);
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
-	    }
-	    else {
-	    	System.out.println("Compra cancelada.");
-	    }
-	}
-
-	public static void main(String[] args) throws CsvValidationException {
-		
-		createCSVFile();
-		
-		// Instancia lista de filmes
+	public static ArrayList<Filme> criarCatalogo(String caminhoArquivo) throws CsvValidationException{
 		ArrayList<Filme> listaFilmes = new ArrayList<>();
-		String caminhoArquivo = "ListaFilmes.csv";
+		
 		try (CSVReader reader = new CSVReader(new FileReader(caminhoArquivo))) {
 			String[] linha;
 			boolean primeiraLinha = true;
@@ -207,32 +82,24 @@ public class Main {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		//Cria uma lista com as datas em que os filmes serão exibidos
-		ArrayList<String> listaDatas = new ArrayList<>();
-		listaDatas.add("28/06");
-		listaDatas.add("29/06");
-		listaDatas.add("30/06");
-		
-		//Cria uma lista com os horarios que os filmes serao exibidos
-		ArrayList<String> listaHorarios = new ArrayList<>();
-		listaHorarios.add("14h");
-		listaHorarios.add("16h");
-		listaHorarios.add("18h");
-		
-		ArrayList<Sessao> listaSessoes = new ArrayList<>();
-		//Para cada data da listaDatas é criado uma sessao em cada um dos horarios
-		for (int i=0; i< listaFilmes.size(); i++) {
-			for (int j=0; j < listaDatas.size(); j++) {
-				for (int k=0; k < listaHorarios.size(); k++) {
-					Sessao sessao = new Sessao(listaFilmes.get(i), 6,5, listaHorarios.get(k), listaDatas.get(j));
-					listaSessoes.add(sessao);
-				}
-			}
+		return listaFilmes;
+	}
+	
+	public static void main(String[] args){
+		//criacao do arquivo que guarda a disponibilidade dos assentos
+		createCSVFile();
+		// Instancia lista de filmes
+		ArrayList<Filme> listaFilmes = new ArrayList<>();
+		String caminhoArquivo = "ListaFilmes.csv";
+		try {
+			listaFilmes = criarCatalogo(caminhoArquivo);
+		} catch (CsvValidationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
-		
-		Reservar reserva = new Reservar("AssentosReservados.csv");
+		//criacao das sessoes
+		ArrayList<Sessao> listaSessoes = new ArrayList<>();
+		listaSessoes = criarSessoes(listaFilmes);
 		new Entrada(listaSessoes);
 
 	}
